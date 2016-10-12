53df88c29217b3f7b17967ba2cbe0b924c7418b8
Kristian <kristian.exss@haw-hamburg.de>
2016-10-13 00:19:47 +0200

    OGraph hat jetzt mehr funktionierende Anweisungen

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/32/600e03b79090001614f9ab6bb5751df1
@@ -0,0 +1,36 @@
+package aufgabe1;
+
+import edu.uci.ics.jung.graph.util.EdgeType;
+
+public class Kante {
+private String name;
+private double wert;
+private EdgeType typ;
+
+public Kante (String namePar, double wertPar, EdgeType typPar) {
+	name=namePar;
+	wert=wertPar;
+	typ=typPar;
+}
+
+public String getName() {
+	return name;
+}
+
+public double getWert() {
+	return wert;
+}
+
+public void setWert(double wertPar) {
+	wert = wertPar;
+}
+
+public EdgeType getTyp() {
+	return typ;
+}
+
+public void setTyp(EdgeType typPar) {
+	typ = typPar;
+}
+
+}
--- GKAP/src/aufgabe1/OGraph.java
+++ GKAP/src/aufgabe1/OGraph.java
@@ -1,40 +1,119 @@
 package aufgabe1;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.util.EdgeType;
 import edu.uci.ics.jung.graph.util.Pair;
 
 public class OGraph implements Graph<Knoten,Kante> {
+	private List<Knoten> knotenList;
+	private List<Kante> kantenList;
+	
+	public OGraph () {
+		knotenList=new ArrayList<Knoten>();
+		kantenList=new ArrayList<Kante>();
+	}
+	
+	private boolean knotenZuKante(Kante kante, Knoten startKnoten, Knoten zielKnoten) {
+		if (kante==null||startKnoten==null||zielKnoten==null) {
+			return false;
+		}
+		kante.setStartKnoten(startKnoten);
+		kante.setZielKnoten(zielKnoten);
+		return true;
+	}
+	
+	private boolean kontrollierKnoten (Knoten startKnoten, Knoten zielKnoten) {
+		if (startKnoten==null||zielKnoten==null) {
+			return false;
+		}
+		if (!containsVertex(startKnoten)) {
+			addVertex(startKnoten);
+		}
+		if (!containsVertex(zielKnoten)) {
+			addVertex(zielKnoten);
+		}
+		return true;
+	}
+	
 
 	@Override
-	public boolean addEdge(Kante arg0, Collection<? extends Knoten> arg1) {
-		// TODO Auto-generated method stub
+	public boolean addEdge(Kante kante, Knoten startKnoten, Knoten zielKnoten) {
+		if (knotenZuKante(kante, startKnoten, zielKnoten)||kontrollierKnoten(startKnoten,zielKnoten)) {
+			return false;
+		}
+		kantenList.add(kante);
+		return true;
+	}
+
+	@Override
+	public boolean addEdge(Kante kante, Knoten startKnoten, Knoten zielKnoten, EdgeType typ) {
+		if (typ==null||kante==null) {
+			return false;
+		}
+		kante.setTyp(typ);
+		return addEdge(kante,startKnoten,zielKnoten);
+	}
+	
+	@Override
+	public boolean addEdge(Kante kante, Collection<? extends Knoten> knoten) {
+		if(knoten==null||knoten.size()==0) {
+			return false;
+		}
+		Knoten[] knotenArray = knoten.toArray(new Knoten[knoten.size()]);
+		if (knotenArray.length==1) {
+			return addEdge(kante,knotenArray[0],knotenArray[0]);
+		}
+		else {
+			return addEdge(kante,knotenArray[0],knotenArray[1]);
+		}
+	}
+
+	@Override
+	public boolean addEdge(Kante kante, Collection<? extends Knoten> knoten, EdgeType typ) {
+		if (typ==null||kante==null) {
+			return false;
+		}
+		kante.setTyp(typ);
+		return addEdge(kante,knoten);
+	}
+
+	@Override
+	public boolean addVertex(Knoten knoten) {
+		if (knoten==null||containsVertex(knoten)) {
+			return false;
+		}
+		knotenList.add(knoten);
+		return true;
+	}
+
+	@Override
+	public boolean containsEdge(Kante kante) {
+		if (kante==null) {
+			return false;
+		}
+		for (int i=0; i<kantenList.size();i++) {
+			if (kante.equals(kantenList.get(i))) {
+				return true;
+			}
+		}
 		return false;
 	}
 
 	@Override
-	public boolean addEdge(Kante arg0, Collection<? extends Knoten> arg1, EdgeType arg2) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addVertex(Knoten arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean containsEdge(Kante arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean containsVertex(Knoten arg0) {
-		// TODO Auto-generated method stub
+	public boolean containsVertex(Knoten knoten) {
+		if (knoten==null) {
+			return false;
+		}
+		for (int i=0; i<knotenList.size();i++) {
+			if (knoten.equals(knotenList.get(i))) {
+				return true;
+			}
+		}
 		return false;
 	}
 
@@ -45,51 +124,86 @@ public class OGraph implements Graph<Knoten,Kante> {
 	}
 
 	@Override
-	public Kante findEdge(Knoten arg0, Knoten arg1) {
-		// TODO Auto-generated method stub
+	public Kante findEdge(Knoten startKnoten, Knoten zielKnoten) {
+		if (startKnoten==null||zielKnoten==null) {
+			return null;
+		}
+		for (int i=0; i<kantenList.size();i++) {
+			Kante kante = kantenList.get(i);
+			if (kante.getStartKnoten().equals(startKnoten)&&kante.getZielKnoten().equals(zielKnoten)) {
+				return kante;
+			}
+			
+		}
 		return null;
 	}
 
 	@Override
-	public Collection<Kante> findEdgeSet(Knoten arg0, Knoten arg1) {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<Kante> findEdgeSet(Knoten startKnoten, Knoten zielKnoten) {
+		LinkedList<Kante> kanten = new LinkedList<Kante>();
+		if (startKnoten==null||zielKnoten==null) {
+			return kanten;
+		}
+		for (int i=0; i<kantenList.size();i++) {
+			Kante kante = kantenList.get(i);
+			if (kante.getStartKnoten().equals(startKnoten)&&kante.getZielKnoten().equals(zielKnoten)) {
+				kanten.add(kante);
+			}
+			
+		}
+		return kanten;
 	}
 
 	@Override
 	public EdgeType getDefaultEdgeType() {
-		// TODO Auto-generated method stub
-		return null;
+		return EdgeType.UNDIRECTED;
 	}
 
 	@Override
 	public int getEdgeCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		return kantenList.size();
 	}
 
 	@Override
-	public int getEdgeCount(EdgeType arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getEdgeCount(EdgeType typ) {
+		// TODO Effizientere Möglichkeit einbauen, wenn möglich
+		int anzahl=0;
+		if (typ!=null) {
+			for (int i=0;i<kantenList.size();i++) {
+				if (kantenList.get(i).getTyp().equals(typ)) {
+					anzahl++;
+				}
+			}
+		}
+		return anzahl;
 	}
 
 	@Override
-	public EdgeType getEdgeType(Kante arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public EdgeType getEdgeType(Kante kante) {
+		if (kante==null) {
+			return null;
+		}
+		return kante.getTyp();
 	}
 
 	@Override
 	public Collection<Kante> getEdges() {
-		// TODO Auto-generated method stub
-		return null;
+		return kantenList;
 	}
 
 	@Override
-	public Collection<Kante> getEdges(EdgeType arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Collection<Kante> getEdges(EdgeType typ) {
+		ArrayList<Kante> kanten = new ArrayList<Kante>();
+		if (typ==null) {
+			return kanten;
+		}
+		for (int i=0;i<kantenList.size();i++) {
+			Kante kante=kantenList.get(i);
+			if (kante.getTyp().equals(typ)) {
+				kanten.add(kante);
+			}
+		}
+		return kanten;
 	}
 
 	@Override
@@ -154,18 +268,6 @@ public class OGraph implements Graph<Knoten,Kante> {
 
 	@Override
 	public boolean removeVertex(Knoten arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addEdge(Kante arg0, Knoten arg1, Knoten arg2) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean addEdge(Kante arg0, Knoten arg1, Knoten arg2, EdgeType arg3) {
 		// TODO Auto-generated method stub
 		return false;
 	}
